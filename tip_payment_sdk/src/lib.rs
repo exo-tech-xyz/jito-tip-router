5bf6f0b33c887663edec88679692a22a9c7595ed
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-12-18 14:17:15 -0500

    WIP add tip payment SDK

---
+++ tip_payment_sdk/src/lib.rs
@@ -0,0 +1,13 @@
+
+
+pub const CONFIG_ACCOUNT_SEED: &[u8] = b"CONFIG_ACCOUNT";
+pub const TIP_ACCOUNT_SEED_0: &[u8] = b"TIP_ACCOUNT_0";
+pub const TIP_ACCOUNT_SEED_1: &[u8] = b"TIP_ACCOUNT_1";
+pub const TIP_ACCOUNT_SEED_2: &[u8] = b"TIP_ACCOUNT_2";
+pub const TIP_ACCOUNT_SEED_3: &[u8] = b"TIP_ACCOUNT_3";
+pub const TIP_ACCOUNT_SEED_4: &[u8] = b"TIP_ACCOUNT_4";
+pub const TIP_ACCOUNT_SEED_5: &[u8] = b"TIP_ACCOUNT_5";
+pub const TIP_ACCOUNT_SEED_6: &[u8] = b"TIP_ACCOUNT_6";
+pub const TIP_ACCOUNT_SEED_7: &[u8] = b"TIP_ACCOUNT_7";
+
+pub const HEADER_SIZE: usize = 8;
